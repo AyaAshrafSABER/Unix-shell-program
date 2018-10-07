5ef7d2e8086c2bb07b0f17305de8cef8ab45ccea
ayasaberosman <ayasaberosman@gmail.com>
2018-10-07 18:23:16 +0200

    intialize project

---
+++ C-Shell/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+void sig_handler(int signum)
+{
+    printf("Received signal %d\n", signum);
+}
+
+int main()
+{
+    signal(SIGINT, sig_handler);
+    //sleep(10); // This is your chance to press CTRL-C
+    return 0;
+}
