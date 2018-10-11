e832cae29a3752395c5883ae3c16c60473dc7740
ayasaberosman <ayasaberosman@gmail.com>
2018-10-11 16:39:37 +0200

    Add a commend to the program

--- C-Shell/main.c
+++ C-Shell/main.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
+/*
+ * this is the main function of the program */
 int main()
-{   remove("log.txt");
+{   remove("log.txt"); // if there is a log file this delete it to write the new running of the shell
     while(1) {
         line = readInstruction();
         background = 0;
@@ -11,10 +13,11 @@ int main()
         free(arguments);
     }
 }
-
+/*
+ * this function is response for executing the incomming command*/
 void execute() {
     if (arguments == NULL) {
-        printf("Try to enter the input again");
+        printf("Try to enter the input again \n");
     } else if (strcmp(arguments[0], "exit") == 0) {
         exit(0);
     }
@@ -23,25 +26,27 @@ void execute() {
     if (child_pid == 0) {
         /* Never returns if the call is successful */
         execvp(arguments[0], arguments);
-        printf("This won't be printed if execvp is successul\n");
+        printf("This won't be printed if the command was right\n");
     } else if (background == 0){
-        waitpid(child_pid, &stat_loc, WUNTRACED);
+        waitpid(child_pid, &stat_loc, WUNTRACED); //this man that the parent proccess wait its child to end its execution
 
     } else {
-        signal(SIGCHLD,signalHandler);
+        signal(SIGCHLD,signalHandler); // active the signal of the process which is running in the background
     }
 
 }
 
 void signalHandler(int signum)
 {
+    //check if the process is runing in the background
     if (signum==SIGCHLD && background == 1) {
+        //get the time which the inturrupt is happen
         time_t mytime = time(NULL);
         char *time_str = ctime(&mytime);
         time_str[strlen(time_str) - 1] = '\0';
         FILE *fp;
         fp = fopen("log.txt", "a");
-        /* write 10 lines of text into the file stream*/
+        /* append lines of text into the file stream*/
         fprintf(fp, "Child process was terminated   %s \n", time_str);
         /* close the file*/
         fclose(fp);
--- C-Shell/parser.cpp
+++ C-Shell/parser.cpp
@@ -5,19 +5,19 @@
 #define LSH_RL_BUFSIZE 1024 //the size of the line.
 #define LSH_TOK_BUFSIZE 64 // Maximum size of arguments to be token.
 #define LSH_TOK_DELIM " \t\r\n\a"
-
+//This function handle if the & placed at the end of the first paramenter
 char* remove_and_sign(char* s, int** background){
     int i =0;
-    for(i = 0; s[i] != '\0'; ++i);
-    if(s[i-1] == '&') {
+    for(i = 0; s[i] != '\0'; ++i); // find the lenghth of the input string
+    if(s[i-1] == '&') { // check if the last input = "&"
         int bufsize = LSH_TOK_BUFSIZE;
         char* newstring =malloc(sizeof(char) * bufsize);
         int j = 0;
         for (j = 0; j < i -1; ++j) {
-            newstring[j] = s[j];
+            newstring[j] = s[j];  // copy the input string to new string without &
         }
-        newstring[j] = '\0';
-        **background = 1;
+        newstring[j] = '\0';  // add thr null caracter
+        **background = 1; //make the command run in the back ground
         return  newstring;
     }
     return s;
@@ -26,7 +26,7 @@ char* remove_and_sign(char* s, int** background){
 //read instruction to be executed from the user which maximum size = 1024
 char* readInstruction(void){
     int bufsize = LSH_RL_BUFSIZE;
-    char *line = malloc(sizeof(char) * bufsize);
+    char *line = malloc(sizeof(char) * bufsize); // intialize new line in heap
     char ch;
     int i = 0;
     printf("Shell> ");
@@ -49,28 +49,33 @@ char **get_arguments(char *line, int* background) {
         fprintf(stderr, "lsh: allocation error\n");
        return NULL;
     }
-
-    token = strtok(line, LSH_TOK_DELIM); /*Trim all spaces and tabes  from the line*/
-    token = remove_and_sign(token, &background);
-    while (token != NULL) {
-        if (strcmp(token, "&") == 0) {
-            *background = 1;
-        } else {
-            tokens[position] = token;
-            position++;
-        }
-        if (position >= bufsize) {
-            bufsize += LSH_TOK_BUFSIZE;
-            tokens = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens) {
-                fprintf(stderr, "lsh: allocation error\n");
-                return NULL;
+    int i = 0;
+    for(i = 0; line[i] != '\0'; ++i); // find the lenghth of the input string
+    if (i > 1) { //check if the user enter a word or not
+        token = strtok(line, LSH_TOK_DELIM); /*Trim all spaces and tabes  from the line*/
+        token = remove_and_sign(token, &background);
+        while (token != NULL) {
+            if (strcmp(token, "&") == 0) {
+                *background = 1;
+            } else {
+                tokens[position] = token;
+                position++;
             }
+            if (position >= bufsize) { // check the size to reallocate the place of the arguments array
+                bufsize += LSH_TOK_BUFSIZE;
+                tokens = realloc(tokens, bufsize * sizeof(char *));
+                if (!tokens) {
+                    fprintf(stderr, "lsh: allocation error\n");
+                    return NULL;
+                }
+            }
+            token = strtok(NULL, LSH_TOK_DELIM);
         }
-        token = strtok(NULL, LSH_TOK_DELIM);
+        tokens[position] = NULL;
+        return tokens;
+    } else {
+        return NULL;
     }
-    tokens[position] = NULL;
-    return tokens;
 }
 
 
